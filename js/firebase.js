7271a2c062eaf09dbbfed2378439d66734377d89
محمد || M O H A M D <abnadlep80@gmail.com>
2026-02-14 00:24:08 +0300

    Add Firebase configuration and initialization
    Initialized Firebase with configuration settings.

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+// ضع بيانات Firebase الخاصة بك هنا
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
